e97907439bb386dce6a93cdfbc9b9d04113c76e7
马雷 <zziamalei@qq.com>
2019-01-15 12:46:15 +0800

    保存帖子，上传文件

--- js/common.js
+++ js/common.js
@@ -1,1 +1,7 @@
 let apiPath='http://localhost:8888';
+
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
